452510c6494181f9bc0336e219c6bf6dd8d0472d
Manuel Saintotte <msaintotte@gmail.com>
2025-11-10 14:10:50 +0100

    Actualizo configuración y agrego carpeta android

---
+++ android/app/src/main/java/com/pudis/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.pudis.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.pudis.app',
   appName: 'PUDIS',
   webDir: 'dist',
-  server: {
-    url: 'https://2d0b62aa-e26c-4763-9bcc-6732686d810f.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
 };
 
 export default config;
